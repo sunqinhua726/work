60bc8b48f69335446ae666cbb2aa8172721ba54a
sunqinhua726 <92302463+sunqinhua726@users.noreply.github.com>
2023-06-05 16:43:35 +0800

    Add files via upload

---
+++ work/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "   *****   \n"
+                 " *       * \n"
+                 "*         *\n"
+                 "*  o   o  *\n"
+                 "*    ^    *\n"
+                 " * \\___/ *\n"
+                 "   *****   \n";
+    return 0;
+}
+
